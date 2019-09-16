00c1871aaa0de5a9d08f7a3898ea364f20e8e358
meihan99 <2279659273@qq.com>
2019-09-16 21:50:28 +0800

    first commit

---
+++ noSingle.java
@@ -0,0 +1,11 @@
+class Person {
+   public static void sayHello() {
+       System.out.println("hello");
+   }
+ }
+class Example13 {
+     public static void main(String[] args) {
+         Person.sayHello();
+   }
+}
+ 
