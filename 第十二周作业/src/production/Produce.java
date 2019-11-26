9ab2d003a1c33f8b8ef316fb518bce2bb387be04
meihan99 <55042027+meihan99@users.noreply.github.com>
2019-11-26 22:49:18 +0800

    第十二周作业

---
+++ 第十二周作业/src/production/Produce.java
@@ -0,0 +1,7 @@
+package production;
+
+
+import user.*;
+public abstract class Produce {
+	public abstract User produceUser();
+}
---
+++ 第十二周作业/src/production/TelcomeUserProduce.java
@@ -0,0 +1,9 @@
+package production;
+
+
+import user.*;
+public class TelcomUserProduce extends Produce{
+	public User produceUser(){
+		return new TelcomUser("15515158296");
+	}
+}
